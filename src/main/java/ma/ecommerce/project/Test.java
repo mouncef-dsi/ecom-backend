6b1522c38aa830af84ba440d16e6f3e7c8d7453f
Ayoub LAHKIM <ayoub_lahkim_astro@ext.carrefour.com>
2024-10-09 17:01:40 +0100

    understand primitives

--- src/main/java/ma/ecommerce/project/Test.java
+++ src/main/java/ma/ecommerce/project/Test.java
@@ -6,8 +6,13 @@ import ma.ecommerce.project.javaws.Chien;
 
 public class Test {
     public static void main(String[] args) {
-        Chat chat = new Chat("chatton",2,6);
 
-        System.out.println(chat.computeAge());
+        double l = 0;  // double c'est une primitive
+        System.out.println(l);
+
+
+        Double x = null; // on peut lui affecter null lorsque c'est en majuscule
+        System.out.println(x);
+
     }
 }
